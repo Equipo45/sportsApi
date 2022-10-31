93c5212d3faf738a24f0ac0e39ed47fdc99ba43c
Ismael Ruiz <ismiruiz@gmail.com>
2022-10-31 13:16:08 +0100

    add error handling and Read.me#

--- src/database/workout.js
+++ src/database/workout.js
@@ -2,51 +2,72 @@ const DB = require("./db.json");
 const { saveToDataBase } = require("./utils");
 
 const getAllWorkouts = () => {
-    return DB.workouts
-}
+  try {
+    return DB.workouts;
+  } catch (error) {
+    throw { status: 500, message: error };
+  }
+};
 
-const getOneWorkouts = (id) =>{
-    //Find index si no lo ecuentra pone -1
-    const notExist = DB.workouts.findIndex((workout) => workout.name === workout.name )> -1;
-    if(!notExist){
-        return;
+const getOneWorkouts = (id) => {
+  //Find index si no lo ecuentra pone -1
+  try {
+    const notExist =
+      DB.workouts.findIndex((workout) => workout.name === workout.name) > -1;
+    if (!notExist) {
+      return;
     }
-    const singleworkout = DB.workouts.findIndex((workout) => workout.id === id)
+    const singleworkout = DB.workouts.findIndex((workout) => workout.id === id);
     return singleworkout;
-}
+  } catch (error) {
+    throw { status: error?.status || 500, message: error?.message || error };
+  }
+};
 //This function is use for saving the new workout in case is not repeated
 const postNewWorkout = (newWorkout) => {
-    const isAlreadyAdded = DB.workouts.findIndex((workout) => workout.name === newWorkout.name) > -1;
-    if(isAlreadyAdded){
-        console.log("Workout already created")
-        return;
-    }
-    DB.workouts.push(newWorkout)
-    saveToDataBase(DB)
+  const isAlreadyAdded =
+    DB.workouts.findIndex((workout) => workout.name === newWorkout.name) > -1;
+  if (isAlreadyAdded) {
+    throw {
+      status: 400,
+      message: `The workout ${newWorkout} already exist!`,
+    };
+  }
+  try {
+    DB.workouts.push(newWorkout);
+    saveToDataBase(DB);
     return newWorkout;
-}
+  } catch (error) {
+    throw { status: 500, message: error?.message || error };
+  }
+};
 
-const updatedNewWorkout = (id,updateData) => {
-    const notExist = DB.workouts.findIndex((workout) => workout.name === newWorkout.name) > -1;
-    if(!notExist){
-        console.log("Workout doesn´t extist")
-        return;
+const updatedNewWorkout = (id, updateData) => {
+  try {
+    const notExist =
+      DB.workouts.findIndex((workout) => workout.name === newWorkout.name) > -1;
+    if (!notExist) {
+      console.log("Workout doesn´t extist");
+      return;
     }
     const updatedWorkout = {
-        ...DB.workout[notExist],
-        ...updateData,
-        updateAt : new Date().toLocaleString("es-ES",{timeZone:"GMT+2"})
-    }
+      ...DB.workout[notExist],
+      ...updateData,
+      updateAt: new Date().toLocaleString("es-ES", { timeZone: "GMT+2" }),
+    };
     //Aqui esta en memoria
-    DB.workouts[notExist] = updatedWorkout
+    DB.workouts[notExist] = updatedWorkout;
     //Aqui esta en disco
-    saveToDataBase(DB)
+    saveToDataBase(DB);
     return updatedWorkout;
-}
+  } catch (error) {
+    throw { status: error?.status || 500, message: error?.message || error };
+  }
+};
 
 module.exports = {
-    getAllWorkouts,
-    postNewWorkout,
-    getOneWorkouts,
-    updatedNewWorkout
- }
+  getAllWorkouts,
+  postNewWorkout,
+  getOneWorkouts,
+  updatedNewWorkout,
+};
--- src/services/workoutService.js
+++ src/services/workoutService.js
@@ -1,44 +1,67 @@
-const { v4:uuid } = require("uuid")
-const Workout = require("../database/workout")
+const { v4: uuid } = require("uuid");
+const Workout = require("../database/workout");
 
 const getAllWorkouts = () => {
+  try{
     const allworkouts = Workout.getAllWorkouts();
     return allworkouts;
-}
+  } catch(error) {
+    throw error
+  }
+  
+};
 
 const getOneWorkouts = (workoutId) => {
+  try{
     const oneworkout = Workout.getOneWorkouts(workoutId);
     return oneworkout;
-}
+  } catch(error) {
+    throw error
+  }
+  
+};
 
 const postOneWorkouts = (newWorkouts) => {
-    //We load the workout , the unique id and both dates
-    const postworkout = {
-        ...newWorkouts,
-        id:uuid(),
-        createdAt: new Date().toLocaleString("es-ES",{timeZone:"GMT+2"}),
-        updatedAt: new Date().toLocaleString("es-ES",{timeZone:"GMT+2"})
-    }
-    //Esto nos devuelve el json que metemos directamente
-    const createWorkout = Workout.postNewWorkout(postworkout)
+  //We load the workout , the unique id and both dates
+  const postworkout = {
+    ...newWorkouts,
+    id: uuid(),
+    createdAt: new Date().toLocaleString("es-ES", { timeZone: "GMT+2" }),
+    updatedAt: new Date().toLocaleString("es-ES", { timeZone: "GMT+2" }),
+  };
+  //Esto nos devuelve el json que metemos directamente
+  try {
+    const createWorkout = Workout.postNewWorkout(postworkout);
     return createWorkout;
-}
+  } catch(error) {
+    throw error
+  }
+  
+};
 
-const updateOneWorkouts = (workoutId,updateData) => {
-    const updateworkout = Workout.updateData(workoutId,updateData);
+const updateOneWorkouts = (workoutId, updateData) => {
+  try{
+    const updateworkout = Workout.updateData(workoutId, updateData);
     return updateworkout;
-}
+  } catch(error){
+    throw error
+  }
+};
 
 const deleteOneWorkouts = (workoutId) => {
+  try{
     const deleteworkout = Workout.deletedId(workoutId);
     return deleteworkout;
-}
-
+  } catch(error) {
+    throw error
+  }
+  
+};
 
 module.exports = {
-    getAllWorkouts,
-    getOneWorkouts,
-    postOneWorkouts,
-    updateOneWorkouts,
-    deleteOneWorkouts
-}
+  getAllWorkouts,
+  getOneWorkouts,
+  postOneWorkouts,
+  updateOneWorkouts,
+  deleteOneWorkouts,
+};
